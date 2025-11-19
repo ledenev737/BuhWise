8766c9f1238d8c3f8d30e21308c30ab9ee354ae5
Aleksandr Ledenev <54019259+ledenev737@users.noreply.github.com>
2025-11-19 12:02:41 +0100

    Cleanup: remove generated build files and add .gitignore

---
+++ src/BuhWise.App/Models/Operation.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace BuhWise.Models
+{
+    public class Operation
+    {
+        public long Id { get; set; }
+        public DateTime Date { get; set; }
+        public OperationType Type { get; set; }
+        public Currency SourceCurrency { get; set; }
+        public double SourceAmount { get; set; }
+        public Currency TargetCurrency { get; set; }
+        public double TargetAmount { get; set; }
+        public double Rate { get; set; }
+        public double? Commission { get; set; }
+        public double UsdEquivalent { get; set; }
+        public string? ExpenseCategory { get; set; }
+        public string? ExpenseComment { get; set; }
+    }
+}
